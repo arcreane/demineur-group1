83ac076de479839c6cac18baf14d6444904805f0
Jon-23 <jonathanknv2@gmail.com>
2021-01-21 16:50:16 +0100

    Correction test

---
+++ DemineurV2/Case/Flag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demineur
+{
+    class Flag
+    {
+        public static int Flags_available { get; internal set; }
+        public static int Flags_available_max { get; internal set; }
+    }
+}
---
+++ DemineurV2/Jeu.cs
@@ -0,0 +1,24 @@
+﻿using Demineur;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemineurV2
+{
+    class Jeu
+    {
+        const int hauteurmax = 10;
+        const int largeurmax = 10;
+        static void Main(string[] args)
+        {
+            Lancement_Du_Jeu();
+        }
+        static void Lancement_Du_Jeu()
+        {
+            Tab Tableau = new Tab();
+            Tableau.CreeTableau(hauteurmax, largeurmax);    
+        }
+    }
+}
