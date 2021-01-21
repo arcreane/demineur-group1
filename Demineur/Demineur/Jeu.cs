c91bab87cd94d3aae0babbfd5331aa2b76676b8e
Jon-23 <jonathaknv2@gmail.com>
2021-01-21 16:01:02 +0100

    Ajout d'un script pour Box

---
+++ Demineur/Demineur/Jeu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demineur
+{
+    class Jeu
+    {
+        const int hauteurmax = 10;
+        const int largeurmax = 10;
+        static void Main(string[] args)
+        {
+            Lancement_Du_Jeu();
+        }
+        static void Lancement_Du_Jeu()
+        {
+            Tab Tableau = new Tab();
+            Tableau.CreeTableau(hauteurmax, largeurmax);    
+        }
+    }
+}
